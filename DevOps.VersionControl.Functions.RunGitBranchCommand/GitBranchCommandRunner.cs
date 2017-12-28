5debd1d164e145ec7264f76604d01f861048e0de
Christopher Dorst <chris@dorst.email>
2017-12-28 16:09:54 -0600

    Add GitBranchCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitBranchCommand/GitBranchCommandRunner.cs
@@ -0,0 +1,14 @@
+﻿using static DevOps.VersionControl.Functions.RunGitCommand.GitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitBranchCommand
+{
+    public static class GitBranchCommandRunner
+    {
+        private const string branch = nameof(branch);
+
+        public static void Branch(string directory, params string[] arguments)
+            => Git(directory,
+                command: branch,
+                arguments);
+    }
+}
